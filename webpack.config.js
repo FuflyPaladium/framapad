220249f6658b8447892b2dc78b8344e577b3343f
DIDELOT Tim <61084278+FuflyPaladium@users.noreply.github.com>
2021-11-05 14:25:28 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -122,6 +122,10 @@ const config = {
     hot: true,
     writeToDisk: true,
     compress: true,
+    allowedHosts: [
+      'irrplay.xyz',
+      'irrplay.xyz:3000'
+    ],
     port: 3000
   },
   devtool: 'eval-source-map',
